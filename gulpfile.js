f2f3d0e698c32ce32365f1a6d8621aed6cf73895
Julio Di Nicola <juliodinicola@gmail.com>
2015-07-28 13:07:59 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+var pkg = require('./package.json');
+var fs = require('fs');
+var gulp = require('gulp');
+var path = require('path');
+var del = require('del');
+var gutil = require('gulp-util');
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
+var header = require('gulp-header');
+var watch = require('gulp-watch');
+
+var banner = ['/**',
+    ' * <%= pkg.name %> - <%= pkg.description %>',
+    ' * @version v<%= pkg.version %>',
+    ' * @link <%= pkg.homepage %>',
+    ' * @author <%= pkg.author.name %>',
+    ' * @license <%= pkg.license %>',
+    ' */',
+''].join('\n');
+
+var build_dir = 'dist';
+
+gulp.task('clean', function(cb) {
+    del([build_dir + '/**/*'], { force: true },cb);
+});
+
+gulp.task('move', ['clean'], function() {
+    gulp.src([
+        '!./node_modules/**/*',
+        '!./' + build_dir + '/**/*',
+        './assets/**/*',
+        './**/*.hbs'
+    ], { base: './' })
+    .pipe(gulp.dest(build_dir))
+    .on('finish', function() {
+        fs.writeFileSync(build_dir + '/package.json', JSON.stringify({ name:pkg.name, version:pkg.version }), null, '\t');
+    });
+});
+
+gulp.task('less', function() {
+    return gulp.src(['less/main.less'])
+        .pipe(less({
+            paths: [path.join(__dirname, 'less', 'includes')]
+        }))
+        .pipe(minifyCss())
+        .pipe(concat('sigmund.min.css'))
+        .pipe(header(banner, { pkg : pkg }))
+        .pipe(gulp.dest(build_dir + '/assets/css'));
+});
+
+gulp.task('watch', function() {
+    watch('less/**/*.less', function() {
+        gulp.start('less');
+    });
+
+    watch(['!./' + build_dir + '/**/*', './assets/**/*', './**/*.hbs'], function() {
+        gulp.start('move');
+    });
+});
+
+gulp.task('build', ['move', 'less']);
