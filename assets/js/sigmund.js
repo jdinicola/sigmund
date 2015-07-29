455f1038f9d476cc0d37adef44a0272e36302e0f
Julio Di Nicola <juliodinicola@gmail.com>
2015-07-29 16:39:42 +0200

    Added FB comments count

--- assets/js/sigmund.js
+++ assets/js/sigmund.js
@@ -1,5 +1,18 @@
 $(function() {
 
-    
+    $('.comments-count').each(function() {
+
+        url = 'https://graph.facebook.com/v2.3/?fields=share{comment_count}&id=' + $(this).data('href');
+        count = 0;
+
+        $.getJSON(url, function(data) {
+
+            if ( data.hasOwnProperty('share') ) count = data.share.comment_count;
+
+        });
+
+        $(this).append(count);
+
+    });
 
 });
